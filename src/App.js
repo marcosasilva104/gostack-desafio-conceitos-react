0ddc6c7f8a456135179ff0cc0cdf85c150580cc0
marcosasilva104 <62699037+marcosasilva104@users.noreply.github.com>
2020-04-14 01:20:19 -0300

    Desafio 03 - ReactJS

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+  //const [repositories, setRepositories] = useState([]);
+      api.get('repositories').then(response => {
+          setRepositories(response.data);
+      });
+  }, []);
+  
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Novo repository ${Date.now()}`,
+      url: 'http://testereactjs.com.br',
+      techs: ['Node.js', 'ReactJS']
+  })
+
+  //const repository = response.data;
+
+  setRepositories([ ...repositories, response.data ]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+      {repositories.map(repository => (
+        <li key={repository.id}>
+          {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+      ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
